cd06f5e9d2db5f2b1b1eee6dde80c1738e2f7423
Mohammad Amanour Rahman <126825646+aman0311x@users.noreply.github.com>
2024-07-13 05:20:42 +0600

    Add files via upload

---
+++ Data Structure/Sparse Table.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mx 50002
+int table[mx][16], arr[mx];
+void build(int n)
+{
+    for(int i=1; i<=n; i++)
+        table[i][0] = arr[i];
+    for(int j=1; (1<<j)<=n; j++){
+        for(int i=1; i + (1<<j) -1 <= n ; i++)
+            table[i][j] = max(table[i][j-1], table[i+(1<<(j-1))][j-1]);
+    }
+}
+int query(int low, int high)
+{
+    int len = high-low+1;
+    int k = log2(len);
+    return max( table[low][k], table[ high - (1<<k) +1][k] );
+}
+
+int main() {
+    int n, q, a, b;
+    cin >> n >> q;
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
+    }
+    build(n);
+    int ans = 0;
+    while(q--){
+        int a, b;
+        cin >> a >> b;
+        if(a > b) swap(a,b);
+        if(abs(a - b) == 1) ans++;
+        else{
+            int mxe = query(a + 1, b - 1);
+            if(mxe <= arr[a]){
+                ans++;
+            }
+        }
+    }
+    cout << ans << '\n';
+
+}
