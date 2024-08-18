a8fc04721b6548c75f18dbf3e642f2a4729734ae
Mohammad Amanour Rahman <126825646+aman0311x@users.noreply.github.com>
2024-08-18 23:47:59 +0600

    Add files via upload

---
+++ String/LPS.cpp
@@ -0,0 +1,44 @@
+//https://toph.co/p/convert-string-into-palindrome
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define llu unsigned long long
+int lcs(string &s, string &t){
+    int n = s.length();
+    int m = t.length();
+
+    vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(s[i - 1] == t[j - 1]){
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.length();
+    string t = s;
+    reverse(t.begin(), t.end());
+    int m = lcs(s,t);
+
+    cout << (n - m) << '\n';
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1, tc = 0;
+    cin >> t;
+    while(t--){
+        cout << "Case " << ++tc << ": ";
+        solve();
+    }
+}
